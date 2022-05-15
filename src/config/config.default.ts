ccc70782764ef2db42da231e502a043ef1f986b3
朱军 <13072474469@163.com>
2022-05-15 14:09:05 +0800

    init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1652592597006_9506',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
